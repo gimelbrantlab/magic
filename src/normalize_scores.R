5c25d1b3678ac371abbe4a38216bc504b9465cd0
Sachit Saksena <sachitdsaksena@utexas.edu>
2017-01-10 17:58:43 +0000

    Update normalize_scores.R

--- src/normalize_scores.R
+++ src/normalize_scores.R
@@ -1,10 +1,9 @@
 
-
 # Normalizes the scores output from bigwig_to_scores based on
 # either a control bigwig file or the length of each interval.
 normalize_scores <- function(scores_file, output_file, dropped_file,
                              mark, gene_region, control_file = NA, 
-                             drop_percent = 0.05) {
+                             drop_percent = 0.05, drop_abs = 1) {
   
   # Opens scores file and converts NA values to 0
   scores <- read.csv(scores_file, sep = "\t", header = TRUE)
@@ -18,12 +17,12 @@ normalize_scores <- function(scores_file, output_file, dropped_file,
     control[is.na(control)] <- 0
     control <- control %>% mutate(percentile = rank(sum) / length(sum))
     
-    # Removes indices in the given bottom percentile from both data frames
-    genes_to_keep <- control$name[control$percentile > drop_percent]
-    genes_to_remove <- control$name[control$percentile <= drop_percent]
-    control <- control[control$name %in% genes_to_keep,]
-    scores <- scores[scores$name %in% genes_to_keep,]
-    
+    # Removes indices in the given bottom percentile from both data frames and below a specified absolute mean value
+    genes_to_keep <- control$name[(control$percentile > drop_percent) & (control$mean >= drop_abs)]
+    genes_to_remove <- control$name[(control$percentile <= drop_percent) | (control$mean < low_abs)]
+    control <- control[!control$name %in% genes_to_remove,]
+    scores <- scores[!scores$name %in% genes_to_remove,]
+ 
     # Writes list of dropped genes to file
     for (gene in genes_to_remove) {
       cat_f(paste(gene, "\t", mark, "_", gene_region, sep = ""), dropped_file)
