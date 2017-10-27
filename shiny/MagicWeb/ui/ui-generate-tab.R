afb17e094dcecc59dc0eefbfaa0f36b22d57d7f2
Sachit Saksena <sachitdsaksena@utexas.edu>
2017-10-27 10:53:49 -0500

    Update generate file IO

--- shiny/MagicWeb/ui/ui-generate-tab.R
+++ shiny/MagicWeb/ui/ui-generate-tab.R
@@ -1,25 +1,56 @@
+# Copyright (C) 2017 Dana-Farber Cancer Institute Inc.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Questions, comments and concerns can be directed to
+#   Alexander Gimelbrant: alexander_gimelbrant@dfci.harvard.edu
+#   Sebastien Vigneau: Sebastien_Vigneau@dfci.harvard.edu
+#   Svetlana Vinogradova: Svetlana_Vinogradova@dfci.harvard.edu
+#   Henry Ward: henry.neil.ward@gmail.com
+#   Sachit Saksena: sachitdsaksena@utexas.edu
+
+#######
+# GENERATE.R
+#######
+
 tabPanel(value = "generate",
-          title = "Select model",
+          title = "Model",
          sidebarLayout(
-              # mainPanel = mainPanel(NULL),
+        # mainPanel = mainPanel(NULL),
           mainPanel = mainPanel(
              tabsetPanel(
               id = "generatePlots",
                          tabPanel("Summary Table",
                                 dataTableOutput("modelTbl")
-                         ),
-                         tabPanel("Precision-recall plot",
-                                      plotOutput("modelPlot",
-                                                 height = 480,
-                                                 width = 700
-                                      )
-                                      
-                                    ))
+                         )
+                         # tabPanel("Precision-recall plot",
+                         #              plotOutput("modelPlot",
+                         #                         height = 480,
+                         #                         width = 700
+                         #              )
+                         #              
+                         #            )
+              )
               ),
               sidebarPanel = sidebarPanel(
               tabsetPanel(
                 id = "options",
-                tabPanel("New",   
+                tabPanel("New", 
+               tags$h4(HTML("<u>Select model output directory</u>")),
+               shinyDirButton("generateOutput", "Choose output directory", "Upload"),
+               br(),
+               br(),
                fileInput('trainingFile', 'Upload Processed ChIP-seq TSV File',
                          accept = acceptable_file_types),
                fileInput('validationSet', 'Upload an external validation set if you have one (optional):',
@@ -35,7 +66,7 @@ tabPanel(value = "generate",
                  label = "Alternatively, select a training file not packaged with MaGIC",
                  accept = acceptable_file_types
                ),
-               textInput('targetFeature', 'Target Feature (the name of the column with class labels, normally MAE/BAE)',
+               textInput('targetFeature', "Target Feature (the name of the column with class labels, normally 'status'",
                          placeholder = 'Enter name of target column'),
                numericInput("trainingPercent",
                             "Fraction of Data to use for Training:", 80,
@@ -81,11 +112,9 @@ tabPanel(value = "generate",
                choices = model_names, 
                multiple = TRUE
              ),
-           h3(HTML("<u>Or:</u>")),
-           directoryInput(
-             "modelDir",
-             label = "Select model directory:",
-             value = "~"),
+           h4(HTML("<u>Or:</u>")),
+           tags$h4(HTML("<u>Select folder containing models</u>")),
+           shinyDirButton("modelDir", "Choose directory with models", "Upload"),
              h3(HTML("Now:")),
              model_name <- get_names("input.modelDir", pattern = "*.rds"),
              selectizeInput(
