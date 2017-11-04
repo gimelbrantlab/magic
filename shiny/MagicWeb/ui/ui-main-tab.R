d96dfea0f87feed26e24569ecd1207b5476a3f83
Sachit Saksena <sachitdsaksena@utexas.edu>
2017-11-04 17:13:40 -0500

    Update "How to use shinyMaGIC"

--- shiny/MagicWeb/ui/ui-main-tab.R
+++ shiny/MagicWeb/ui/ui-main-tab.R
@@ -35,21 +35,55 @@ tabPanel(value = "main_panel",
              h2(HTML("Welcome to MaGICs: <br> <b>M</b>ono<b>a</b>llelic <b>G</b>ene <b>I</b>nference from <b>C</b>hromatin for <b>s</b>hiny")),
              actionButton("get_started", "Get started"),
              br(),
-             h2(HTML("<u>Overview</u>")),
+             h3(HTML("<u>Overview</u>")),
              tags$p(HTML(
                "MaGIC 2.0 is a graphical user interface for predicting allele specific expression in
                polyclonal cell samples. The program leverages the predictive power of chromatin signatures
                to generate educated inferences on monoallelic state of genes within a polyclonal sample via
                statistical learning methods and monoclonal allelic bias determination.")),
+             h3(
+               HTML("MaGIC offers three distinct functionalities: <br>")
+             ),
+             h4(
+               HTML("1. Process")
+             ),
+             tags$p(HTML("Process is a shiny wrapper for the command line utility <b>process.R</b>.
+                         This program functions to convert bigWig data from processed ChIP-Seq reads and
+                         convert them to mapped scores compatible with analysis of MAE status through
+                         analyze.R and model generation through generate.R. Process directly passes the
+                         bigWig files inputted to this page and outputs *.tsv* files with ChIP-Seq region
+                         scores by gene. Navigate to the <b>Process</b> tab to use this feature.")),
+             h4(
+               HTML("2. Analyze")
+             ),
+             tags$p(HTML("Analyze is a shiny wrapper for the command line utility <b>analyze.R</b>.
+                         This program functions to provide predictions for a sample using a
+                         previously trained model to determine status for genes within that sample. Simply provide
+                         a file of ChIP-Seq enrichment scores from process.R and retrieve predictions.
+                         Navigate to the <b>Analyze</b> tab to use this feature.")),
+             h4(
+               HTML("3. Generate")
+             ),
+             tags$p(HTML("We encourage the use of MaGIC 2.0 to improve upon published predictive models
+                         for inferring MAE to improve predictive reliability and indicate possible mechanistic
+                         implications of epigenetic signatures in MAE. To this end, Generate is a shiny wrapper
+                         for the command line utility <b>generate.R</b>. This program functions to allow users to
+                         input training data based on monoclonal allelic imbalance data and novel epigenetic signature
+                         data to create models with a variety of different algorithms to further optimize the effort to
+                         characterize cell-cell allele-specific heterogeneity. Navigate to the <b>Model</b> tab to use
+                         this feature.")),
              p(),
              h3(
                HTML("How to provide MaGIC with your data. <br>")
                ),
+                h4(
+                  HTML("Provide data to 'Process' tab.")
+                ),
                  tags$p(HTML("<b>Files</b> takes a correctly formatted '.txt' or '.tsv' file with names of chromatin marks
                   and control files and automatically selects the correct files from the input directory."),
                         p(HTML("To use this method input a file following the format below with fields seperated by tabs
                      in a text file format:<br>
-                               <table style='width:100%'>
+                               <table style='width:50%'>
                                <tr>
                                <th>mark name</th>
                                <th>mark file</th>
@@ -67,44 +101,59 @@ tabPanel(value = "main_panel",
                                </tr>
                                </table>")),
                         br(),
-                        p("The program will now automatically pick the filenames you provided when you
-                          move to the processing tab.")
+                        p("Put this *.txt* or *.tsv* file in the directory with the *.bigWig* files you want to process.
+                        The program will now automatically pick the filenames you provided when you
+                          move to the processing tab."),
+                        p("You then set the *output* 
+                          directory of where the processed data tables will be stored.")
                ),
-             h3(
-               HTML("MaGIC offers three distinct functionalities: <br>")
-             ),
              h4(
-               HTML("1. Process")
+               HTML("Provide data to 'Model' tab.")
              ),
-             tags$p(HTML("Process is a shiny wrapper for the command line utility <b>process.R</b>.
-                         This program functions to convert bigWig data from processed ChIP-Seq reads and
-                         convert them to mapped scores compatible with analysis of MAE status through
-                         analyze.R and model generation through generate.R. Process directly passes the
-                         bigWig files inputted to this page and outputs *.tsv* files with ChIP-Seq region
-                         scores by gene. Navigate to the <b>Processing</b> tab to use this feature.")),
+             h5(
+               HTML("Generating a new model:")
+             ),
+             tags$p(HTML("In the 'Model' tab, you can generate new models using processed marks. 
+                         You can provide data from Process.R output by 
+                         selecting the file named 'joined_scores_percentile' from the designated output folder. 
+                         You then set an output folder for models that you generate and tables showing performance of models.")),
+             tags$p(HTML("To validate the model with an external dataset, you can provide another table of processed marks and gene status.")),
+             h5(
+                HTML("Using an existing model:")
+             ),
+             tags$p(HTML("In the 'Model' tab, you can also use models packaged with the MaGIC download by selecting the model
+                         from the menu in the 'Existing' tab in the Model section. You can also provide a model you generated
+                         previously (in the form of an *.rds* file) by selecting the folder containing these models and picking
+                         models from a dropdown menu.")),
              h4(
-               HTML("2. Analyze")
-             ),
-             tags$p(HTML("Analyze is a shiny wrapper for the command line utility <b>analyze.R</b>.
-                     This program functions to provide predictions for a sample using a
-                     previously trained model to determine status for genes within that sample. Simply provide
-                     a file of ChIP-Seq enrichment scores from process.R and retrieve predictions.
-                     Navigate to the <b>Analysis</b> tab to use this feature.")),
-             h4(
-               HTML("3. Generate")
-             ),
-             tags$p(HTML("We encourage the use of MaGIC 2.0 to improve upon published predictive models
-                         for inferring MAE to improve predictive reliability and indicate possible mechanistic
-                         implications of epigenetic signatures in MAE. To this end, Generate is a shiny wrapper
-                         for the command line utility <b>generate.R</b>. This program functions to allow users to
-                         input training data based on monoclonal allelic imbalance data and novel epigenetic signature
-                         data to create models with a variety of different algorithms to further optimize the effort to
-                         characterize cell-cell allele-specific heterogeneity. Navigate to the <b>Model Generation</b> tab to use
-                         this feature.")),
+               HTML("Provide data to 'Analyze' tab.")
+               ),
+             tags$p(HTML("To use models to get predictions in the Analyze, first provide the dataset you
+                         want predictions for. Then select an output directory for the predictions.
+                         If you want to filter your predictions for expression and gene length, you can 
+                         upload a file with the following format:
+                          <table style='width:20%'>
+                          <tr>
+                         <th>gene</th>
+                         <th>expression</th>
+                         <th>length</th>
+                         </tr>
+                         <tr>
+                         <td>A1BG</td>
+                         <td>10</td>
+                         <td>2400</td>
+                         </tr>
+                         <tr>
+                         <td>A1CF</td>
+                         <td>2400</td>
+                         <td>2500</td>
+                         </tr>
+                         </table>"
+                         )),
+             tags$p(HTML("Then select the length of genes you want to filter.")),
              width = 12
              ),
-           mainPanel(img(src='opening_slide.png', align='left', width='900px', height='530px')),
-           position = "left"
+           mainPanel(NULL)
            )
          #### File method
            )
